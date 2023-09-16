7d64b312db1d79be7b53a65ce5cb51574cf84141
mattshrew <mattshrewy@gmail.com>
2023-09-16 15:03:46 -0400

    finished layout

--- main.py
+++ main.py
@@ -4,14 +4,27 @@ parts = ["part-1", "part-2", "part-3", "part-4", "part-5"]
 
 text = "no "
 newline = "\n"
+pages = [("/", "Home")]
 
 page = f"""
-# hiiii # {{: .header}}
+<|layout|columns=1fr auto 1fr|class_name=container align_columns_center|width=100%|height=10vh|
+<|part|class_name=title|
+<|InjuryInspect|text|width=30px|height=30px|>
+|>
+|>
 
-<|{{text}}|>
-
+<|layout|columns=1fr 6fr 3fr|class_name=injury|
+<|part|class_name=select|
+<|side.png|image|width=80%|class_name=small_image|>
+<|side.png|image|width=80%|class_name=small_image|>
+<|side.png|image|width=80%|class_name=small_image|>
+|>
+<|part|class_name=diagram|
+<|side.png|image|width=100%|class_name=big_image|>
 {newline.join([f'<| |button|id={part}|on_action=info|>' for part in parts])}
-
+|>
+|>
+    
 """
 
 # {''.join([f'<| |button|id={part}|on_action=info|>' for part in parts])}
